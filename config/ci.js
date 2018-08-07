c2806486cb0ec04c1f4f9484723f6c419596ec7e
Sagar Bhattacharya <sagarbhattacharya@Sagars-MacBook-Pro.local>
2018-08-08 04:05:54 +0530

    upate commit

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "577392138636-3e18378ueo41l8ktambechn5uuoall9o.apps.googleusercontent.com",
+  googleClientSecret: "EjWqMrcfjxJlsBZ4LkUqOnno",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379"
+};
